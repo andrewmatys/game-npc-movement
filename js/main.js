89f4596c8047cec8f7a9da9bc5929aeeb9bae7ca
andrewmatys <andrewmatysiak@hotmail.com>
2025-03-05 10:04:03 +1100

    Added more restrictive movement within canvas border.

--- js/main.js
+++ js/main.js
@@ -8,30 +8,36 @@ const CANVAS_HEIGHT = canvas.height = 1000;
 const numberOfEnemies = 100;
 const enemiesArray = [];
 
+let gameFrame = 0;
 
-const enemyImage = new Image();
-enemyImage.src = '../sprites/enemy1.png';
+
 
 class Enemy {
   constructor(){
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
-    this.speed = (Math.random() * 4) - 2;
+    this.image = new Image();
+    this.image.src = '../sprites/enemy1.png';
+    // this.speed = (Math.random() * 4) - 2;
     this.spriteWidth = 293;
     this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width); // brackets contents prevents going off screen
+    this.y = Math.random() * (canvas.height - this.height);
+    this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 4 + 7); // Math.floor lets gameFrame be divisble by % (line 33)
   }
 
   update(){
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x += Math.random() * 3 - 1.5;
+    this.y += Math.random() * 3 - 1.5;
+    if (gameFrame % this.flapSpeed === 0 ){ // each bat rolls against GLOBAL ever-increasing gameFrame variable. (flapSpeed)
+      this.frame > 4 ? this.frame = 0 : this.frame++;
+    }
   }
 
   draw(){
-    // ctx.fillRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(enemyImage, this.x, this.y);
-
+    // this.x, this.y from update()
+    ctx.drawImage(this.image, (this.frame * this.spriteWidth), (0), (this.spriteWidth), (this.spriteHeight), this.x, this.y, this.width, this.height);
   }
 };// Enemy{}
 
@@ -42,16 +48,13 @@ class Enemy {
 
 
 for (let i = 0; i < numberOfEnemies; i++){
-  // const enemy = new Enemy();
-  // enemiesArray.push(enemy);
   enemiesArray.push(new Enemy());
-
 }
 
 
 
 
-
+//animation loop
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   
@@ -60,8 +63,11 @@ function animate(){
     enemy.draw();
   }
 
+  gameFrame++;
+
   requestAnimationFrame(animate)
 }
+
 
 animate();
 
@@ -69,3 +75,5 @@ animate();
 
 //W: 1758
 //H: 155
+
+// 1h:47m
